6bd894a4d822595475a0fc89879fa9c4bd919e9e
Ilya <61752920+ilyadudiak@users.noreply.github.com>
2023-04-11 13:03:48 +0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import myBtn from './App';
+import './index.css'
+import MyApp from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MyApp />
+
+  </React.StrictMode>,
+)
+
